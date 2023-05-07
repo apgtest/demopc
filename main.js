194334ca14bdf544952ab8098123c2da1ffa02d6
apgtest <angelptest@gmail.com>
2023-05-06 20:40:46 -0500

    demopc

---
+++ main.js
@@ -0,0 +1,28 @@
+/*Activar y desactivar el menu*/
+const  nav = document.querySelector(".nav");
+const navlist = document.querySelector(".nav-list");
+const menu = document.querySelector(".menu-mb");
+const cancel = document.querySelector(".cancel-mb");
+
+/*Seccion de Menu*/
+
+menu.onclick = () =>{
+    navlist.classList.add("active");
+    menu.classList.add("hidden");
+}
+
+
+/*Remueve el nav bar al ejecutarse*/
+nav.addEventListener("click", (e) =>{
+    if(!e.target.matches(".nav a")) return false;
+    navlist.classList.remove("active");
+    menu.classList.remove("hidden");
+});
+
+/*Cierra el menu bar*/
+cancel.onclick = () =>{
+    navlist.classList.remove("active");
+    menu.classList.remove("hidden");
+}
+
+
